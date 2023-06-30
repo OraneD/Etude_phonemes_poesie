69aa002e2de7597627705a76a3d9713bfbe12628
Orane Dufour <orane.dufour@gmail.com>
2023-06-30 22:36:44 +0200

    travail prose

---
+++ scripts/stats_consonnes/prose_consonnes.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 30 22:28:17 2023
+
+@author: orane
+"""
+
+import csv 
+from pathlib import Path
+import re 
+import matplotlib.pyplot as plt
+import numpy as np
+
+liste_phonemes = ["a", "A", "b", "C", "k", "d", "e", "E", "f", "g", "H", "i", "j", "J", "t", "Z", "z", "e~", "w", "@", "s", "R", "2", "u", "y", "o", "O", "9", "v", "m", "n", "N", "S"]
+liste_consonnes_sourdes = ["p", "t", "k", "f", "s" "S"]
+liste_consonnes_sonores = ["b", "d", "g", "v", "z" "j", "l","R", "n" "N", "m"]
+couple_k_g = ["k" , "g"]
+couple_f_v = ["f", "v"]
+couple_p_b_m = ["p", "b", "m"]
+couple_s_z = ["s", "z"]
+couple_S_Z = ["S", "Z"]
+couple_t_d = ["t", "d"]
+liste_consonnes =  ["p", "t", "k", "f", "s", "S","b", "d", "g", "v", "z", "j", "l","R", "n", "N", "m", "w", "H"]
+#Pourcentage par phonème
+def stats_phonemes(fichier):
+
+                dico_consonnes = {"k, g" : 0, "f, v" : 0, "H" : 0, "s, z" : 0, "S, Z" : 0, "R" : 0, "l" : 0, "j" : 0,"p, b, m" : 0, "w" : 0, "t, d" : 0, "n" : 0}
+
+                with open(fichier, "r") as file:
+                    reader = file.read()
+                    for phoneme in liste_consonnes :
+                        if phoneme in couple_k_g  and len(re.findall(phoneme, reader)) != 0 :
+                            dico_consonnes["k, g"] += len(re.findall(phoneme, reader))
+                            
+                        elif phoneme in couple_f_v and len(re.findall(phoneme, reader)) != 0 :
+                            dico_consonnes["f, v"] += len(re.findall(phoneme, reader))
+                            
+                        elif phoneme in couple_p_b_m and len(re.findall(phoneme, reader)) != 0 :
+                            dico_consonnes["p, b, m"] += len(re.findall(phoneme, reader))
+                            
+                        elif phoneme in couple_s_z and len(re.findall(phoneme, reader)) != 0 :
+                            dico_consonnes["s, z"] += len(re.findall(phoneme, reader))
+                            
+                        elif phoneme in couple_S_Z and len(re.findall(phoneme, reader)) != 0 :
+                            dico_consonnes["S, Z"] += len(re.findall(phoneme, reader))
+                        
+                        elif phoneme in couple_t_d and len(re.findall(phoneme, reader)) != 0:
+                            dico_consonnes["t, d"] += len(re.findall(phoneme, reader))
+                             
+                        elif len(re.findall(phoneme, reader)) != 0 :
+                            dico_consonnes[phoneme] += len(re.findall(phoneme, reader))
+                            
+    
+                names = list(dico_consonnes.keys())
+                values = [round((v / sum(dico_consonnes.values())) * 100, 2) for v in dico_consonnes.values()]
+                return names, values
+
+
+                
+Nhugo, Vhugo = stats_phonemes("../../resultats/transcriptions_prose/hugo_prose.txt")
+Nbaudelaire, Vbaudelaire = stats_phonemes("../../resultats/transcriptions_prose/baudelaire_prose.txt")
+Nmusset, Vmusset = stats_phonemes("../../resultats/transcriptions_prose/musset_prose.txt")
+Nlamartine, Vlamartine = stats_phonemes("../../resultats/transcriptions_prose/lamartine_prose.txt")
+
+
+N = 4
+ind = np.arange(len(Nhugo))
+width = 0.20
+
+xvals = Vhugo
+bar1= plt.bar(ind, xvals, width, color = "tab:blue")
+
+yvals = Vbaudelaire
+bar2 = plt.bar(ind + width, yvals, width, color = "tab:orange")
+
+zvals = Vmusset
+bar3 = plt.bar(ind+width*2, zvals, width, color = "tab:green")
+
+uvals = Vlamartine
+bar4 = plt.bar(ind+width*3, uvals, width, color = "tab:red")
+
+
+plt.ylabel("%")
+plt.title("Pourcentage de chaque consonne (prose)")
+
+plt.xticks(ind+width, Nhugo)
+plt.ylim(0,20)
+plt.legend( (bar1, bar2, bar3, bar4), ('Hugo', 'Baudelaire', 'Musset', "Lamartine") )
+plt.show()
+
+
+def camembert(labels, valeurs, titre) :
+    plt.figure(figsize = (8,8))
+    plt.pie(valeurs, 
+            labels = labels, 
+            normalize = True, 
+            autopct = '%1.1f%%',
+            #pctdistance = 0.7, labeldistance = 1.4,
+            shadow = True)
+    plt.title(titre, fontsize=14)
+    plt.show()
+
+camembert(Nhugo, Vhugo, "Répartition consonnes pour Hugo (prose)")
+camembert(Nbaudelaire, Vbaudelaire, "Répartition consonnes pour Baudelaire (prose)")
+camembert(Nmusset, Vmusset, "Répartition consonnes pour Musset (prose)")
+camembert(Nlamartine, Vlamartine, "Répartition consonnes pour Lamartine (prose)")
+
+def histogramme_sup(x1, x2, x3, x4) :
+    
+    
+    
+    valeurs_tous = []
+    
+    for i in range(len(x1)) :
+        j = 0
+        j += round((x1[i] + x2[i] + x3[i] + x4[i])/4, 2)
+        valeurs_tous.append(j)
+    print(sum(valeurs_tous))
+      
+    plt.ylabel("%")
+    width = 0.05
+    ind = np.arange(len(Nhugo))
+    bar1= plt.bar(ind, valeurs_tous, color = "tab:blue")
+    plt.xticks(ind+width, Nhugo)
+    plt.ylim(0,20)
+    plt.title("Pourcentage consonnes l'ensemble du corpus (prose)")
+    plt.show()
+    
+    plt.figure(figsize = (8,8))
+    plt.pie(valeurs_tous, 
+            labels = Nhugo, 
+            normalize = True, 
+            autopct = '%1.1f%%',
+            #pctdistance = 0.7, labeldistance = 1.4,
+            shadow = True)
+    plt.title("Pourcentage consonnes sur l'ensemble du corpus (prose)")
+
+    plt.show()
+    
+
+    
+histogramme_sup(Vhugo, Vbaudelaire, Vmusset, Vlamartine)
--- scripts/transcription/transcription_prose.py
+++ scripts/transcription/transcription_prose.py
@@ -8,10 +8,14 @@ Created on Thu Jun 29 13:54:00 2023
 
 import re 
 
-def transcription_prose(fichier):
+
+def transcription_prose(fichier, output):
     
     transcription_phonetique = ""
     mots_inconnus = []
+    #Listes pour les règles 
+    voyelles_sampa = ["i", "e", "E", "a", "A", "O", "o", "u", "y", "2", "9", "@", "e~", "a~", "o~", "9~", "C", "H", "j","w"]
+    voyelles = ["a", "e", "i", "o", "u", "y", "é", "è","à"]
     
     
     with open(fichier, "r") as f :
@@ -35,29 +39,55 @@ def transcription_prose(fichier):
                 lignes = dico.readlines()
                 for ligne in lignes :
                     entry = ligne.split("\t")[0]
-                    phonetique = ligne.split("\t")[1]
+                    phonetique = ligne.split("\t")[1].strip("\n")
                     
                     if mot == ("\n") or mot == "." or mot == "?" or mot == "!":
                         transcription_phonetique += mot
                         break
                         
                     if mot.strip() == entry :
-                        
                         print(mot, entry)
-                        transcription_phonetique += phonetique.strip("\n")
-                        break
+                        
+                        #Règles pour les liasions - "s"
+                        if len(liste_mots) > 1 and (mot[-1] == "s" or mot[-1] == "x") and liste_mots[0][0] in voyelles :
+                            transcription_phonetique += phonetique + "z"
+                            break
+                        
+                        #Règles pour les liasons "-t"
+                        elif  len(liste_mots) > 1 and (mot[-1] == "t" or mot[-1] == "d") and liste_mots[0][0] in voyelles :
+                            transcription_phonetique += phonetique + "t"
+                            break
+ 
+                        else :
+                            transcription_phonetique += phonetique.strip("\n")
+                            break
                         
                     elif entry == "***" : 
                         transcription_phonetique += "XXXX "
                         if mot not in mots_inconnus :
                             mots_inconnus.append(mot)
                         break
+    with open("mot_inconnus.txt", "w") as mi :
+        for mot in mots_inconnus :
+            mi.write(mot + "\n")
     
-    print(mots_inconnus)
-    print(transcription_phonetique)
+    with open(output, "w") as f :
+        f.write(transcription_phonetique)
+        
+    print(str(fichier) + " transcrit")
+    
                 
         
         
-        
 
-transcription_prose("../../Corpus/Baudelaire/baudelaire_ecole_utf8.txt")
+#transcription_prose("../../Corpus/Baudelaire/baudelaire_ecole_utf8.txt", 
+                    #"../../resultats/transcriptions_prose/baudelaire_prose.txt")
+
+#transcription_prose("../../Corpus/Hugo/hugo_quot_utf8.txt",
+                   # "../../resultats/transcriptions_prose/hugo_prose.txt")
+
+#transcription_prose("../../Corpus/Musset/musset_dupuis_utf8.txt", 
+                    #"../../resultats/transcriptions_prose/musset_prose.txt")
+
+transcription_prose("../../Corpus/LaMartine/lamartine_destinees_utf8.txt",
+                    "../../resultats/transcriptions_prose/lamartine_prose.txt")
